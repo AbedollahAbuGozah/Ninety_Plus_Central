8536e1cfe88f63a804e38feadd312efffb6ad8f1
Abedullah Abugozah <abedabugozah@gmail.com>
2024-06-28 19:23:26 +0300

    enhancements

--- src/database/migrations/2024_06_27_225512_create_transactions_table.php
+++ src/database/migrations/2024_06_27_225512_create_transactions_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('status');
             $table->timestamp('transaction_date')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->text('details')->nullable();
-            $table->foreignId('user_id')->constrained('users')->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->timestamps();
         });
     }
