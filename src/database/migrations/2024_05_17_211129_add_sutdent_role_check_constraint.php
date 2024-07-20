55af0ad4ffe6325341d5a08ad3df53ae1f66a715
Abedullah Abugozah <abedabugozah@gmail.com>
2024-07-20 20:19:27 +0300

    fix in student_role_check migration

--- src/database/migrations/2024_05_17_211129_add_sutdent_role_check_constraint.php
+++ src/database/migrations/2024_05_17_211129_add_sutdent_role_check_constraint.php
@@ -8,48 +8,48 @@ return new class extends Migration
     public function up()
     {
         // Create the trigger for INSERT
-        DB::unprepared('
-            CREATE TRIGGER check_student_role_before_insert
-            BEFORE INSERT ON course_students
-            FOR EACH ROW
-            BEGIN
-                IF NOT EXISTS (
-                    SELECT 1
-                    FROM role_user
-                    JOIN roles ON role_user.role_id = roles.id
-                    WHERE role_user.user_id = NEW.student_id
-                    AND roles.name = "student"
-                ) THEN
-                    SIGNAL SQLSTATE "45000"
-                    SET MESSAGE_TEXT = "Student role constraint failed.";
-                END IF;
-            END
-        ');
-
-        // Create the trigger for UPDATE
-        DB::unprepared('
-            CREATE TRIGGER check_student_role_before_update
-            BEFORE UPDATE ON course_students
-            FOR EACH ROW
-            BEGIN
-                IF NOT EXISTS (
-                    SELECT 1
-                    FROM role_user
-                    JOIN roles ON role_user.role_id = roles.id
-                    WHERE role_user.user_id = NEW.student_id
-                    AND roles.name = "student"
-                ) THEN
-                    SIGNAL SQLSTATE "45000"
-                    SET MESSAGE_TEXT = "Student role constraint failed.";
-                END IF;
-            END
-        ');
+//        DB::unprepared('
+//            CREATE TRIGGER check_student_role_before_insert
+//            BEFORE INSERT ON course_students
+//            FOR EACH ROW
+//            BEGIN
+//                IF NOT EXISTS (
+//                    SELECT 1
+//                    FROM role_user
+//                    JOIN roles ON role_user.role_id = roles.id
+//                    WHERE role_user.user_id = NEW.student_id
+//                    AND roles.name = "student"
+//                ) THEN
+//                    SIGNAL SQLSTATE "45000"
+//                    SET MESSAGE_TEXT = "Student role constraint failed.";
+//                END IF;
+//            END
+//        ');
+//
+//        // Create the trigger for UPDATE
+//        DB::unprepared('
+//            CREATE TRIGGER check_student_role_before_update
+//            BEFORE UPDATE ON course_students
+//            FOR EACH ROW
+//            BEGIN
+//                IF NOT EXISTS (
+//                    SELECT 1
+//                    FROM role_user
+//                    JOIN roles ON role_user.role_id = roles.id
+//                    WHERE role_user.user_id = NEW.student_id
+//                    AND roles.name = "student"
+//                ) THEN
+//                    SIGNAL SQLSTATE "45000"
+//                    SET MESSAGE_TEXT = "Student role constraint failed.";
+//                END IF;
+//            END
+//        ');
     }
 
     public function down()
     {
-        // Drop the triggers
-        DB::unprepared('DROP TRIGGER IF EXISTS check_student_role_before_insert');
-        DB::unprepared('DROP TRIGGER IF EXISTS check_student_role_before_update');
+//        // Drop the triggers
+//        DB::unprepared('DROP TRIGGER IF EXISTS check_student_role_before_insert');
+//        DB::unprepared('DROP TRIGGER IF EXISTS check_student_role_before_update');
     }
 };
